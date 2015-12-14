60633c70a09967a444951f0d623f79362c1d873c
Ilona Brand <theilonabrand@gmail.com>
2015-12-14 16:35:05 -0500

    Clickable popups to view Bronx demographic data

--- map.js
+++ map.js
@@ -3,6 +3,7 @@
     var dataLayer=undefined;
    // var query= 1 //"stabilized"
     var query=0; //"complaint"
+    var map, demographicData, coreData, geojsonData;
 
     var defaultStyle = {
       color: 'black', 
@@ -14,10 +15,14 @@
       weight: 3,
     }
 
-    // initialize the map
-    var map = L.map('map').setView([40.778, -73.942], 11);
     $(function() {
-      render(gyear);
+      // initialize the map
+      map = L.map('map').setView([40.778, -73.942], 11);
+            // load a tile layer
+      L.tileLayer('http://{s}.tiles.mapbox.com/v3/heatseeknyc.jjl743fc/{z}/{x}/{y}.png', {
+                attribution: 'Map Data © OpenStreetMap contributors, CC-BY-SA, Tile Set © Mapbox, Complaint Data © NYC Open Data',
+                maxZoom: 18
+      }).addTo(map);
       $( "#slider" ).slider({
         position: 'topright',
         min: 2010,
@@ -43,26 +48,39 @@
         //map.removeLayer(dataLayer)
         render(gyear);
       });
+
+      $.getJSON("data/Income_Bronx2013Zip.json", function(loaded) {
+        demographicData = loaded;
+        $.getJSON("data/neighborhoods.geojson", function(loaded) {
+          geojsonData = loaded;
+          $.getJSON("data/jointdata.json", function(loaded) {
+            coreData = loaded;
+            render(gyear);
+          });
+        });
+      });
     });
 
-    function yearChanged(year){
-      //console.log(year);
-      gyear = year;
-      render(gyear);
+    function render(year){    
+      var oldDataLayer = dataLayer;
+      addDataToMap(geojsonData, coreData, map, year, query);
+      
+      if (oldDataLayer!=undefined){
+        console.log('old',oldDataLayer)
+        console.log('current',dataLayer)
+        map.removeLayer(oldDataLayer); 
+      }  
     }
-
-    // load a tile layer
-    L.tileLayer('http://{s}.tiles.mapbox.com/v3/heatseeknyc.jjl743fc/{z}/{x}/{y}.png', {
-              attribution: 'Map Data © OpenStreetMap contributors, CC-BY-SA, Tile Set © Mapbox, Complaint Data © NYC Open Data',
-              maxZoom: 18
-    }).addTo(map);
-
     // load geojson layer
-    function addDataToMap(geojsondata, zipcodedata, map, year, query) {
-      dataLayer = L.geoJson(geojsondata, {
+    function addDataToMap(geojsonData, zipcodedata, map, year, query) {
+      dataLayer = L.geoJson(geojsonData, {
         onEachFeature: function(feature, layer){
           // popup information about the demographic of the zipcode
-          layer.bindPopup("hello hi")
+          var popupHtml = getPopupContent(feature);
+          //console.log(popupHtml)
+          var popup = L.popup()
+            .setContent(popupHtml)
+          layer.bindPopup(popup);
           layer.on('mouseover', function (e){
             layer.setStyle(hoverStyle);
           });
@@ -77,27 +95,29 @@
       dataLayer.addTo(map);
     }
 
-    function render(year){
-      $.getJSON("data/neighborhoods.geojson", function(geojsondata) {
-        $.getJSON("data/jointdata.json", function(jointdata) {
-          var oldDataLayer = dataLayer;
-          addDataToMap(geojsondata, jointdata, map, year, query);
-          
-          if (oldDataLayer!=undefined){
-            console.log('old',oldDataLayer)
-            console.log('current',dataLayer)
-            map.removeLayer(oldDataLayer); 
-          }  
-        });
-      });
-    }
-
-    function getFeatureStyle(feature, jointdata, year, query){
+    function getPopupContent(feature){
       var targetZip = feature.properties.postalCode;
-      var records = jointdata.features;
+      var html = "Hello World"
+      for (var i in demographicData){
+        rec = demographicData[i]
+        //console.log(rec)
+        if (targetZip == rec.Zip) {
+          html = '<div><strong> Demographics in 2013 </strong></div>' +
+            '<div> Media Income: ' + rec["Median Income"] + '</div>' +
+            '<div> Home Owners: '+ rec["Home Owners"] + '</div>' +
+            '<div> Renters: ' + rec["Renters"] + '</div>' +
+            '<div> % Difference: ' + rec["% Difference HO&R"] +'</div>'
+        }
+      }
+
+      return html;
+    }
+    function getFeatureStyle(feature, cor, year, query){
+      var targetZip = feature.properties.postalCode;
+      var records = cor.features;
       var key=0;
-      console.log(query)
-      console.log("year:", year)
+      //console.log(query)
+      //console.log("year:", year)
       if (query==0){
         for (var i in records) {
           if (targetZip == records[i].zipcode) {
@@ -155,4 +175,10 @@
 
     function getColorRent(density) {
       return density;
+    }
+
+    function yearChanged(year){
+      //console.log(year);
+      gyear = year;
+      render(gyear);
     }
